b0e535f51760f5f35a4d5888edb1b47a9d961604
Lukas Jankauskas <lukasjankauskasthelj@gmail.com>
2023-09-24 23:35:07 +0300

    added more elements

---
+++ Backend/AccountTrial/Domain/Accounting/Bookkeeping/Bookkeeping.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Accounting.Bookkeeping.Transactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Accounting.Bookkeeping
+{
+    public sealed class Bookkeepin
+    {
+        public Credit Credit { get; set; }
+        public Debit Debit { get; set; }
+        public DateTime TransactionDate { get; set; }
+    }
+}
---
+++ Backend/AccountTrial/Domain/Accounting/Bookkeeping/Transactions/TransactionBase.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Accounting.Enumerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Accounting.Bookkeeping.Transactions
+{
+    public abstract class TransactionBase
+    {
+        public int AccountId { get; set; }
+        public int Account { get; set; }
+        public Currency Currency { get; set; }
+    }
+}
